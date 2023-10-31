123f3299c6991836f561fb59e568be4663318611
Atticus Blanco <atticusbrujx@gmail.com>
2023-10-31 13:16:27 -0700

    Part 1 >>>NONOPERATIONAL
    >>>MODIFICATIONS
    --CREATED class file Exam2.java
      -CREATED instance String var `name`
      -CREATED instance int var `age`
      -CREATED instance Scanner obj `input`
      -BUILT constructor `Exam2`
      -STUB method `outputToTerminal`
      -STUB method `chooseFormat`
      -STUB method `prepOutput`
      -STUB method `canVote`
      -STUB method `incAge`
    
    >>>DEFICITS
    --Exam2.java
      -BUILD method `outputToTerminal`
      -BUILD method `chooseFormat`
      -BUILD method `prepOutput`
      -BUILD method `canVote`
      -BUILD method `incAge`
    
    >>>BUGS
    --nothing built
    
    >>>THROWS
    --C:\Users\Gnomatic\IdeaProjects\BlancoAtticusExam2\src\Exam2.java
      -Warning:(4, 20) Private field 'name' is assigned but never accessed
      -Warning:(4, 20) Field can be converted to a local variable
      -Warning:(4, 20) Field 'name' may be 'final'
      -Warning:(5, 17) Private field 'age' is assigned but never accessed
      -Warning:(5, 17) Field can be converted to a local variable
      -Warning:(5, 17) Field 'age' may be 'final'
      -Warning:(6, 21) Private field 'input' is never used
      -Warning:(6, 21) Field 'input' may be 'final'
      -Warning:(12, 9) 'return' is unnecessary as the last statement in a constructor
      -Warning:(15, 48) Parameter 'initialAge' is never used
      -Warning:(16, 9) 'return' is unnecessary as the last statement in a 'void' method
      -Warning:(19, 24) Method 'chooseFormat()' is never used
      -Warning:(20, 9) 'return' is unnecessary as the last statement in a 'void' method
      -Warning:(23, 24) Method 'prepOutput()' is never used
      -Warning:(24, 9) 'return' is unnecessary as the last statement in a 'void' method
      -Warning:(27, 24) Method 'canVote()' is never used
      -Warning:(28, 9) 'return' is unnecessary as the last statement in a 'void' method
      -Warning:(31, 24) Method 'incAge()' is never used
      -Warning:(32, 9) 'return' is unnecessary as the last statement in a 'void' method

---
+++ src/Exam2.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Exam2 {
+    private String name;
+    private int age;
+    private Scanner input = new Scanner(System.in);
+
+    public Exam2(String name, int age){
+        this.name = name;
+        this.age = age;
+        outputToTerminal("Initial age");
+        return;
+    }
+
+    public static void outputToTerminal(String initialAge){
+        return;
+    }
+
+    public static void chooseFormat(){
+        return;
+    }
+
+    public static void prepOutput(){
+        return;
+    }
+
+    public static void canVote(){
+        return;
+    }
+
+    public static void incAge(){
+        return;
+    }
+
+
+
+}
