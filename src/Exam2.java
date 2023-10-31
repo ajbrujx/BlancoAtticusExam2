812e2bffa2579701fe474afefeb87e7a5e967dd2
Atticus Blanco <atticusbrujx@gmail.com>
2023-10-31 13:43:26 -0700

    Part 1 >>>NONOPERATIONAL
    >>>MODIFICATIONS
    --Exam2.java
      -BUILT constructor Exam2
      -PSUEDO method `outputToTerminal`
      -PSUEDO method `chooseFormat`
      -PSUEDO method `prepOutput`
      -PSUEDO method `canVote`
      -PSUEDO method `incAge`
    
    >>>DEFICITS
    --Exam2.java
      -BUILD method `outputToTerminal`
      -BUILD method `chooseFormat`
      -BUILD method `prepOutput`
      -BUILD method `canVote`
      -BUILD method `incAge`
    
    >>>BUGS
    --nothing built
    
    >>>THROWS
    --

--- src/Exam2.java
+++ src/Exam2.java
@@ -7,19 +7,14 @@ public class Exam2 {
 
 
 
-    /*
-     * A constructor with a String parameter and an int parameter that does the following:
-     * Initializes the name instance variable with the value of the String parameter.
-     * Initializes the age instance variable with the value of the int parameter.
-     * Calls the outputToTerminal method and passes the literal string “Initial age “ as the argument.*/
-    public Exam2(String name, int age){
+    public void Exam2(String name, int age){
         this.name = name;
         this.age = age;
-        outputToTerminal("Initial age");
-        return;
+        outputToTerminal("Initial " + age);
     }
 
-    /*A void method named, outputToTerminal with one String parameter that does the following:
+
+    /*
     prints the parameter value on one line.
     Calls the chooseFormat method and "handles" the returned value.
     Calls the prepOutput method, using the return value from the chooseFormat method call above as its argument, and "handles the returned value.
@@ -40,6 +35,7 @@ public class Exam2 {
         return;
     }
 
+
     /*A method named, prepOutput with a return type of String and one parameter of type boolean that does the following:
     If the argument passed to the method is false, returns ONLY the name instance variable.
     If the argument passed to the method is true, returns the instance variable, name, and the instance variable, age,
@@ -48,12 +44,14 @@ public class Exam2 {
         return;
     }
 
+
     /*
     * A method named, canVote with a return type of boolean and no parameters that returns true if age is 18 or greater
     * and false otherwise.*/
     public static void canVote(){
         return;
     }
+
 
     /*A void method named, incAge with no parameters that does the following:
     Increments age instance variable by 1.
@@ -62,7 +60,4 @@ public class Exam2 {
     public static void incAge(){
         return;
     }
-
-
-
 }
